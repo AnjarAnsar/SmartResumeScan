63653e1bc1fb920d7980ade6d7781e670257e554
AnjarAnsar <ianjaransari2003@gmail.com>
2025-07-24 13:25:05 +0530

    Add preview and download features to resume page
    Fixed bugs on generateResume page, added Resume.jsx for final resume review, and enabled resume download functionality for users.

--- resume_frontend/src/pages/GenerateResume.jsx
+++ resume_frontend/src/pages/GenerateResume.jsx
@@ -1,244 +1,297 @@
-import React, { useState, useEffect } from 'react';
-import { FaMagic, FaTrash, FaLightbulb} from 'react-icons/fa';
-import { BiBook } from 'react-icons/bi'; // FIXED: BiBook is from 'react-icons/bi', not 'fa'
-
-import { toast } from 'react-hot-toast';
-import { generateResume } from '../api/ResumeService'; // Adjust the import based on your
-import { useForm, useFieldArray} from "react-hook-form";
-import {FaPlusCircle } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { FaBrain, FaTrash, FaPaperPlane } from "react-icons/fa";
+import { generateResume } from "../api/ResumeService";
+import { BiBook } from "react-icons/bi";
+import { useForm, useFieldArray } from "react-hook-form";
+import { FaPlusCircle } from "react-icons/fa";
+import Resume from "../components/Resume";
 
 const GenerateResume = () => {
-  const { register, handleSubmit, control} = useForm({
-    defaultValues: initialData,
-  })
-
-
-
-
-
-
-  const [description, setDescription] = useState('');
-  const [ loading, setLoading ] = useState(false); // Assuming you have a context for resume generation
-
-  function handleChange(event){
-    //changing personal information
-    const personalInformation ={
-      ...data.personalInformation,
-      [event.target.name]: event.target.value,
-    }
-    setData({
-      ...data,
-      personalInformation: personalInformation,
-    });
-  }
-
-
-
-
-
-
-
-
-
   const [data, setData] = useState({
     personalInformation: {
-      fullName: 'Anzar',
-      email: 'ianjaransari2003@gmail.com',
-      phoneNumber: '3431234567',
-      location: 'xigaz, India',
-      linkedin: 'www.linkedin.com/in/ianjaransari',     
-      github: 'www.github.com/ianjaransari', 
-      portfolio: 'this is protfolio',
+      fullName: "Durgesh Kumar Tiwari",
     },
+    summary: "",
+    skills: [],
+    experience: [],
+    education: [],
+    certifications: [],
+    projects: [],
+    languages: [],
+    interests: [],
   });
 
+  const { register, handleSubmit, control, setValue, reset } = useForm({
+    defaultValues: data,
+  });
 
-    useEffect(() => {
-    console.log(data);
-  },[data]);
-  
+  const [showFormUI, setShowFormUI] = useState(false);
+  const [showResumeUI, setShowResumeUI] = useState(false);
+  const [showPromptInput, setShowPromptInput] = useState(true);
+
+  const experienceFields = useFieldArray({ control, name: "experience" });
+  const educationFields = useFieldArray({ control, name: "education" });
+  const certificationsFields = useFieldArray({
+    control,
+    name: "certifications",
+  });
+  const projectsFields = useFieldArray({ control, name: "projects" });
+  const languagesFields = useFieldArray({ control, name: "languages" });
+  const interestsFields = useFieldArray({ control, name: "interests" });
+  const skillsFields = useFieldArray({ control, name: "skills" });
+
+  //handle form submit
+  const onSubmit = (data) => {
+    console.log("Form Data:", data);
+    setData({ ...data });
+
+    setShowFormUI(false);
+    setShowPromptInput(false);
+    setShowResumeUI(true);
+  };
+
+  const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleGenerate = async () => {
-    // alert('Generate Resume Section based on description:\n\n' + description);
-    // // server call to generate resume can be implemented here
-    // setDescription(''); // Clear the description after generating
+    console.log(description);
+    // server call to get resume
+
     try {
-        setLoading(true);
+      setLoading(true);
       const responseData = await generateResume(description);
-      console.log('Generated Resume:', responseData);
-      setData(responseData); // Assuming responseData contains the resume data
-      toast.success('Resume generated successfully!',{
+      console.log(responseData);
+      reset(responseData.data);
+
+      toast.success("Resume Generated Successfully!", {
         duration: 3000,
-        position: 'top-center',
+        position: "top-center",
       });
+      setShowFormUI(true);
+      setShowPromptInput(false);
+      setShowResumeUI(false);
     } catch (error) {
       console.log(error);
-        toast.error('Failed to generate resume. Please try again.', {
-            duration: 3000,
-            position: 'top-center',
-        });
+      toast.error("Error Generating Resume!");
+    } finally {
+      setLoading(false);
+      setDescription("");
     }
-    finally {
-        setLoading(false);
-        setDescription(''); // Clear the description after generating
-    }   
-    
   };
 
   const handleClear = () => {
-    setDescription('');
+    setDescription("");
   };
-  function showForm(){
-    return <div className="w-full p-10">
-        <h1 className="flex items-center justify-center mb-6 gap-2 text-3xl font-bold text-center">
-          <BiBook className="text-accent" />Resume Form</h1>
-          <div>
-            <p className="py-4">Personal Information</p>
 
-            
-            <div className="grid grid-cols-12 gap-4 mt-2 ">
-              <div className="col-span-12 lg:col-span-6">  { /*  lg:laptop... 6 columns... else 12*/ }
-                <label htmlFor="fullName" className="block mb-2">Full name</label>
-                <input
-                onChange={handleChange} 
-                type="text" 
-                name="fullName"
-                id = "fullName"
-                placeholder="Enter your full name"
-                value={data.personalInformation.fullName}
-                className="input" />
-
-              </div>
-              <div className="col-span-6">
-                <label htmlFor="email" className="block mb-2">Email</label>
-                <input 
-                onChange={handleChange}
-                type="text" 
-                name="email"
-                id = "email"
-                placeholder="Enter here" 
-                value={data.personalInformation.email}
-                className="input input-bordered w-full" />
-
-              </div>
-            </div>
-            <div className="grid grid-cols-12 gap-4 mt-2 ">
-              <div className="col-span-12 lg:col-span-6">  { /*  lg:laptop... 6 columns... else 12*/ }
-                <label htmlFor="phoneNumber" className="block mb-2">Phone Number</label>
-                <input
-                onChange={handleChange} 
-                type="text" 
-                name="phoneNumber"
-                id = "phoneNumber"
-                placeholder="Enter your phoneNumber"
-                value={data.personalInformation.phoneNumber}
-                className="input" />
-
-              </div>
-              <div className="col-span-6">
-                <label htmlFor="location" className="block mb-2">location</label>
-                <input 
-                onChange={handleChange}
-                type="text" 
-                name="location"
-                id = "location"
-                placeholder="Enter location" 
-                value={data.personalInformation.location}
-                className="input input-bordered w-full" />
-
-              </div>
-            </div>
-            <div className="grid grid-cols-12 gap-4 mt-2 ">
-              <div className="col-span-12 lg:col-span-6">  { /*  lg:laptop... 6 columns... else 12*/ }
-                <label htmlFor="linkedin" className="block mb-2">linkedin</label>
-                <input 
-                onChange={handleChange}
-                type="text" 
-                name="linkedin"
-                id = "linkedin"
-                placeholder="Enter your full name"
-                value={data.personalInformation.linkedin}
-                className="input" />
-
-              </div>
-              <div className="col-span-6">
-                <label htmlFor="github" className="block mb-2">GitHub</label>
-                <input  
-                onChange={handleChange}
-                type="text"
-                name="github"
-                id = "github"
-                placeholder="Enter here" 
-                value={data.personalInformation.github}
-                className="input input-bordered w-full" />
-
-              </div>
-            </div>
-            <div className="grid grid-cols-12 gap-4 mt-2 ">
-              <div className="col-span-12 lg:col-span-12">  { /*  lg:laptop... 6 columns... else 12*/ }
-                <label htmlFor="portfolio" className="block mb-2">portfolio</label>
-                <input 
-                onChange={handleChange}
-                type="text" 
-                name="portfolio"
-                id = "portfolio"
-                placeholder="Enter your full name"
-                value={data.personalInformation.portfolio}
-                className="input w-full" />
-
-              </div>
-              
-            </div>
-            
-
-          </div>
-
+  const renderInput = (name, label, type = "text") => (
+    <div className="form-control w-full  mb-4">
+      <label className="label">
+        <span className="label-text text-base-content">{label}</span>
+      </label>
+      <input
+        type={type}
+        {...register(name)}
+        className="input input-bordered rounded-xl w-full bg-base-100 text-base-content"
+      />
     </div>
+  );
+  const renderFieldArray = (fields, label, name, keys) => {
+    return (
+      <div className="form-control w-full mb-4">
+        <h3 className="text-xl font-semibold">{label}</h3>
+        {fields.fields.map((field, index) => (
+          <div key={field.id} className="p-4 rounded-lg mb-4 bg-base-100">
+            {keys.map((key) => (
+              <div key={key}>
+                {console.log(`${name}`)}
+                {renderInput(`${name}.${index}.${key}`, key)}
+              </div>
+            ))}
+            <button
+              type="button"
+              onClick={() => fields.remove(index)}
+              className="btn btn-error btn-sm mt-2"
+            >
+              <FaTrash className="w-5 h-5 text-base-content" /> Remove {label}
+            </button>
+          </div>
+        ))}
+        <button
+          type="button"
+          onClick={() =>
+            fields.append(
+              keys.reduce((acc, key) => ({ ...acc, [key]: "" }), {})
+            )
+          }
+          className="btn btn-secondary btn-sm mt-2 flex items-center"
+        >
+          <FaPlusCircle className="w-5 h-5 mr-1 text-base-content" /> Add{" "}
+          {label}
+        </button>
+      </div>
+    );
+  };
+
+  function showFormFunction() {
+    return (
+      <div className="w-full p-10">
+        <h1 className="text-4xl font-bold mb-6 flex items-center justify-center gap-2">
+          <BiBook className="text-accent" /> Resume Form
+        </h1>
+        <div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="p-6 space-y-6 bg-base-200 rounded-lg text-base-content"
+          >
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {renderInput("personalInformation.fullName", "Full Name")}
+              {renderInput("personalInformation.email", "Email", "email")}
+              {renderInput(
+                "personalInformation.phoneNumber",
+                "Phone Number",
+                "tel"
+              )}
+              {renderInput("personalInformation.location", "Location")}
+              {renderInput("personalInformation.linkedin", "LinkedIn", "url")}
+              {renderInput("personalInformation.gitHub", "GitHub", "url")}
+              {renderInput("personalInformation.portfolio", "Portfolio", "url")}
+            </div>
+
+            <h3 className="text-xl font-semibold">Summary</h3>
+            <textarea
+              {...register("summary")}
+              className="textarea textarea-bordered w-full bg-base-100 text-base-content"
+              rows={4}
+            ></textarea>
+
+            {renderFieldArray(skillsFields, "Skills", "skills", [
+              "title",
+              "level",
+            ])}
+            {renderFieldArray(experienceFields, "Experience", "experience", [
+              "jobTitle",
+              "company",
+              "location",
+              "duration",
+              "responsibility",
+            ])}
+            {renderFieldArray(educationFields, "Education", "education", [
+              "degree",
+              "university",
+              "location",
+              "graduationYear",
+            ])}
+            {renderFieldArray(
+              certificationsFields,
+              "Certifications",
+              "certifications",
+              ["title", "issuingOrganization", "year"]
+            )}
+            {renderFieldArray(projectsFields, "Projects", "projects", [
+              "title",
+              "description",
+              "technologiesUsed",
+              "githubLink",
+            ])}
+
+            <div className="flex gap-3 mt-16  p-4 rounded-xl ">
+              <div className="flex-1">
+                {renderFieldArray(languagesFields, "Languages", "languages", [
+                  "name",
+                ])}
+              </div>
+              <div className="flex-1">
+                {renderFieldArray(interestsFields, "Interests", "interests", [
+                  "name",
+                ])}
+              </div>
+            </div>
+
+            <button type="submit" className="btn btn-primary w-full">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
   }
 
-
-  function ShowInputField(){
-    return <div className="bg-base-100 shadow-xl rounded-xl p-8 w-full max-w-2xl">
-        <h1 className="flex items-center justify-center mb-6 gap-2 text-3xl font-bold text-center">
-          <FaLightbulb className="text-accent" />Enter Your Description</h1>
-
-        <textarea 
-           disabled={loading}   // Disable textarea while loading
-          className="textarea textarea-bordered w-full h-48 resize-none"
-          placeholder="Describe your experience, goals, or background here..."
+  function ShowInputField() {
+    return (
+      <div className="bg-base-200 shadow-lg rounded-lg p-10 max-w-2xl w-full text-center">
+        <h1 className="text-4xl font-bold mb-6 flex items-center justify-center gap-2">
+          <FaBrain className="text-accent" /> AI Resume Description Input
+        </h1>
+        <p className="mb-4 text-lg text-gray-600">
+          Enter a detailed description about yourself to generate your
+          professional resume.
+        </p>
+        <textarea
+          disabled={loading}
+          className="textarea textarea-bordered w-full h-48 mb-6 resize-none"
+          placeholder="Type your description here..."
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         ></textarea>
-
-        <div className="flex justify-center mt-6 gap-4">
+        <div className="flex justify-center gap-4">
           <button
-            disabled={loading || !description.trim()} // Disable button if loading or description is empty
-            className="btn btn-primary gap-2"
+            disabled={loading}
             onClick={handleGenerate}
+            className="btn btn-primary flex items-center gap-2"
           >
             {loading && <span className="loading loading-spinner"></span>}
-            <FaMagic /> Generate
+            <FaPaperPlane />
+            Generate Resume
           </button>
           <button
-            className="btn btn-outline btn-error gap-2"
             onClick={handleClear}
-            disabled={!description.trim()}
+            className="btn btn-secondary flex items-center gap-2"
           >
             <FaTrash /> Clear
           </button>
         </div>
       </div>
+    );
+  }
+  function showResume() {
+    return (
+      <div>
+        <Resume data={data} />
+
+        <div className="flex mt-5 justify-center gap-2">
+          <div
+            onClick={() => {
+              setShowPromptInput(true);
+              setShowFormUI(false);
+              setShowResumeUI(false);
+            }}
+            className="btn btn-accent"
+          >
+            Generate Another
+          </div>
+          <div
+            onClick={() => {
+              setShowPromptInput(false);
+              setShowFormUI(true);
+              setShowResumeUI(false);
+            }}
+            className="btn btn-success"
+          >
+            Edit
+          </div>
+        </div>
+      </div>
+    );
   }
 
-
   return (
-    <div className="mt-5 p-10 flex flex-col gap items-center justify-center font-sas">
-      {        showForm()}
-      
-      {      ShowInputField()}
+    <div className="mt-5 p-10 flex flex-col gap-3 items-center justify-center font-sans">
+      {showFormUI && showFormFunction()}
+      {showPromptInput && ShowInputField()}
+      {showResumeUI && showResume()}
     </div>
-    
-  )
+  );
 };
 
 export default GenerateResume;
